3e60236cae5f5b3658db9c2d23fc59bfe85a6cd7
YangBoZhong <yangbozhone@gmail.com>
2025-03-31 07:03:02 +0800

    更新 MyControls.vb 註解

--- VisualBasicWebForm/App_Code/MyControls.vb
+++ VisualBasicWebForm/App_Code/MyControls.vb
@@ -1,13 +1,22 @@
 ﻿Namespace Controls
-    '透過自訂控件的方式來忽略 <SupportsEventValidation>
-    '避免JS在DropDownList添加選項導致PostBack出現錯誤
-    '來源: https://johanleino.wordpress.com/2011/04/09/revised-cascadingdropdown-causes-invalid-postback-or-callback-argument-error/
+    ''' <summary>
+    ''' 透過自訂控件的方式來忽略 (SupportsEventValidation)
+    ''' 避免JS在DropDownList添加選項導致PostBack出現錯誤
+    ''' </summary>
+    ''' <remarks>
+    ''' 來源: https://johanleino.wordpress.com/2011/04/09/revised-cascadingdropdown-causes-invalid-postback-or-callback-argument-error/
+    ''' </remarks>
     Public Class NoValidationDropDownList
         Inherits System.Web.UI.WebControls.DropDownList
 
     End Class
 
-    '
+    ''' <summary>
+    ''' 覆寫 SelectedValue 屬性解決資料繫結的問題
+    ''' </summary>
+    ''' <remarks>
+    ''' 來源: https://dotblogs.com.tw/jeff377/2008/10/23/5752
+    ''' </remarks>
     Public Class TBDropDownList
         Inherits DropDownList
 
